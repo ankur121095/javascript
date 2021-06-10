eb7b10020eb5d10a98e185f3bb0c1dfc36bf1adb
ankur121095 <ankur.shrivastava2011@gmail.com>
2021-06-10 08:45:38 +0530

    javascript files

---
+++ js/set.js
@@ -0,0 +1,14 @@
+// document.write("Hey");
+
+
+
+document.getElementById("a").innerHTML = "<center><h1>PNINFOSYS GWALIOR</h1></center>"
+
+document.getElementById("b").innerHTML = "<center><h1>Phone:7000846823</h1></center>"
+
+//document.getElementById("a").setAttribute("style","border:10px dotted red");
+
+//document.getElementById("a").removeAttribute("style");
+
+document.querySelector("#b").innerHTML = "<center><h1>Wow</h1></center>";
+document.querySelector(".c").innerHTML = "<center><h1>Ankur</h1></center>";
